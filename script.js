e9655e68763b5e1963b1f2cbefae6a6ee4a706c6
ikenna nwaka <inwaka39@gmail.com>
2025-04-28 18:36:03 +0200

    displayMassage

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let hightscore = 0;
+
+const displayMassage = function (message) {
+  document.querySelector('.message').textContent = message;
+};

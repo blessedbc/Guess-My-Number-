d62cbfc956728ed1b703069956caa02a9ca41195
ikenna nwaka <inwaka39@gmail.com>
2025-04-28 18:40:49 +0200

    addEventListener

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ let hightscore = 0;
 const displayMassage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+
+document.querySelector('.click').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+})
